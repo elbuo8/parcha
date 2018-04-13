bf0aca6d81b2690011f5dd81cd0fc5a8bab28a8d
Lars Jacob <jacob.lars@googlemail.com>
2018-04-13 11:08:46 -0300

    fix whitespace error

--- modules/network/vpc.tf
+++ modules/network/vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "vpc" {
   cidr_block           = "${var.cidr_range}"
   instance_tenancy     = "${var.tenancy}"
-  enable_dns_support   = "${var.enable_dns_support }"
+  enable_dns_support   = "${var.enable_dns_support}"
   enable_dns_hostnames = "${var.enable_dns_hostnames}"
 
   tags {
